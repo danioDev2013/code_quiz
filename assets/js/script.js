ba547e1e298d35605e8d40391a293393026c5dd4
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-01-15 13:58:11 -0800

    started a function, will turn button into submit text, changes to questions and answers, shows timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
 var highScore = document.querySelector('#high-score');
 var quizTimer = document.querySelector('#time-quiz');
 var questTimer = document.querySelector('#time-question');
+var questionAsk = document.querySelector('#question');
+var answersQues = document.querySelector('#question-answer')
+
+var startBtn = document.querySelector('#start-button');
+var submitBtn = document.querySelector('#submit-button');
+
+
+var timers = document.querySelector('#timer');
+
+
+button.addEventListener("click", replaceButtonText);
+
+function replaceButtonText() {
+    document.getElementById("button").innerHTML = "Submit";
+    document.getElementById("question-ask").innerHTML = "Question";
+    document.getElementById("question-answer").innerHTML="answers";
+    document.getElementById("timer").style.visibility = "visible";
+}
 
