5933c18beedc9fe3c8410a7d013196c3ceba48ac
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-01-18 06:08:46 -0800

    added some code back. When you click start the timer shows and button turn to submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,3 +99,26 @@ var quizQuestions = [
         correctAnswer: 'a'
     }, 
 ]
+
+
+var highScore = document.querySelector('#high-score');
+var quizTimer = document.querySelector('#time-quiz');
+var questTimer = document.querySelector('#time-question');
+var questionAsk = document.querySelector('#question');
+var answersQues = document.querySelector('#question-answer')
+
+var startBtn = document.querySelector('#start-button');
+var submitBtn = document.querySelector('#submit-button');
+
+
+var timers = document.querySelector('#timer');
+
+
+button.addEventListener("click", replaceButtonText);
+
+function replaceButtonText() {
+    document.getElementById("button").innerHTML = "Submit";
+    document.getElementById("question-ask").innerHTML = "Question";
+    document.getElementById("question-answer").innerHTML="answers";
+    document.getElementById("timer").style.visibility = "visible";
+}
